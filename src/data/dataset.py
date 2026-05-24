8a12d4886d202c0a4ad4560b48839e5e5810b52d
Hartecky <hofman.bartlomiej@gmail.com>
2026-05-24 11:58:23 +0200

    added filtering questions and sampling followed by valid types of qa validation

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from typing import Optional
 from pathlib import Path
 import json
+import random 
 
 class Question(BaseModel):
     id: str
@@ -13,6 +14,9 @@ class Question(BaseModel):
 
 
 class BioASQDataset:
+
+    VALID_TYPES = {"yesno", "factoid", "list", "summary"}
+
     def __init__(self, path):
         self.path = Path(path)
         self.questions = []
@@ -26,9 +30,20 @@ class BioASQDataset:
             self.questions.append(Question(**record))
 
     def filter_by_type(self, question_type):
+        if question_type not in self.VALID_TYPES:
+            raise ValueError(f"Unknown type: '{question_type}'. Avalaible: {self.VALID_TYPES}")
+    
         filtered_questions = []
         for q in self.questions:
             if q.type == question_type:
                 filtered_questions.append(q)
 
         return filtered_questions
+    
+    def get_sample(self, n, question_type = None):
+        if question_type:
+            pool = self.filter_by_type(question_type)
+        else:
+            pool = self.questions
+
+        return random.sample(pool, n)
