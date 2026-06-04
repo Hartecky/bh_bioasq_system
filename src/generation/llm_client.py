a22eb2b60d5e7f85fc63d23de7b435a46008a615
Hartecky <hofman.bartlomiej@gmail.com>
2026-06-04 08:10:31 +0200

    added ollama client with generate method

---
+++ src/generation/llm_client.py
@@ -0,0 +1,16 @@
+import requests
+
+class OllamaClient():
+    def __init__(self, model="mistral:7b-instruct-q4_0", host="http://localhost:11434"):
+        self.host = host
+        self.model = model
+
+    def generate(self, prompt):
+        payload = {"model": self.model,
+                   "prompt": prompt,
+                   "stream": False}
+        
+        response = requests.post(url=f"{self.host}/api/generate", 
+                                 json=payload)
+        
+        return response.json()
